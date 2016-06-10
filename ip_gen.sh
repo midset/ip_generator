22ef9557882d5691000486ed6e6baa293a010623
Mid Set ~ Hamid <rooter.dz@gmail.com>
2016-06-10 21:26:37 +0200

    Create ip_gen.sh

---
+++ ip_gen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Server Scanner and Joomla Exploiter (JCE)
+# Coded By : Mid Set  
+i=0
+for i in {1..1000} 
+ do 
+	echo $((RANDOM%=255))"."$((RANDOM%=255))"."$((RANDOM%=255))"."$((RANDOM%=255)) >> list.txt
+	let i=$i+1
+	
+	
+done
+echo $i # to count how many ip
